a4ae6d25da8860d987422e156cfae65d4a33bfa9
Balinator <laszlobalazs95@gmail.com>
2018-11-29 13:57:47 +0200

    working with intellij

--- src/test/BoardTest.java
+++ src/test/BoardTest.java
@@ -1,56 +1,55 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
 import src.Board;
 import src.Element;
 
-class BoardTest {
-	private Board board = new Board();
+import static org.junit.Assert.*;
 
-	@Test
-	void test() {
-		assertFalse(board.isSuicide(1));
-	}
-	
-	@Test
-	void testTimerStarted() {
-		assertTrue(board.timer.isRunning());
-	}
-	
-	@Test
-	void testTimerStopped() {
-		board.endGame();
-		assertFalse(board.timer.isRunning());
-	}
-	
-	@Test
-	void testGetScore() {
-		Element e = new Element(null, 4, 0, 0);
-		assertEquals(5,board.getScore(e));
-	}
-	
-	@Test
-	void testGetHigherScore() {
-		Element e = new Element(null, 4, 0, 0);
-		assertNotEquals(10,board.getScore(e));
-	}
-	
-	@Test
-	void testCommitedSuicide() {
-		board.helperFunction(-10, 50);
-		assertTrue(board.commitedSuicide());
-	}
-	
-	@Test
-	void testnotCommitedSuicide() {
-		board.helperFunction(10, 50);
-		assertFalse(board.commitedSuicide());
-	}
+/**
+ * Created by Balinator on 2018. 11. 29..
+ */
+public class BoardTest {
+    private Board board = new Board();
+
+    @Test
+    public void test() {
+        assertFalse(board.isSuicide(1));
+    }
+
+    @Test
+    public void testTimerStarted() {
+        assertTrue(board.timer.isRunning());
+    }
+
+    @Test
+    public void testTimerStopped() {
+        board.endGame();
+        assertFalse(board.timer.isRunning());
+    }
+
+    @Test
+    public void testGetScore() {
+        Element e = new Element(null, 4, 0, 0);
+        assertEquals(5,board.getScore(e));
+    }
+
+    @Test
+    public void testGetHigherScore() {
+        Element e = new Element(null, 4, 0, 0);
+        assertNotSame(10,board.getScore(e));
+    }
+
+    @Test
+    public void testCommitedSuicide() {
+        board.helperFunction(-10, 50);
+        assertTrue(board.commitedSuicide());
+    }
+
+    @Test
+    public void testnotCommitedSuicide() {
+        board.helperFunction(10, 50);
+        assertFalse(board.commitedSuicide());
+    }
 
 }
